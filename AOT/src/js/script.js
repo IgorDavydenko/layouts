29265560d84556326717b3a7532be7cdf7179561
IgorDavydenko <4_me@li.ru>
2021-08-06 12:05:51 +0300

    start layout

--- AOT/src/js/script.js
+++ AOT/src/js/script.js
@@ -1,1 +1,16 @@
-//@@include('')
+// $('.icon-menu').click(function(event) {
+//     $(this).toogleClass('active');
+//     $('menu__body').toogleClass('active');
+//     $('body').toogleClass('lock');
+// })
+
+function ibg() {
+    let ibg = document.querySelectorAll(".ibg");
+    for (let i = 0; i < ibg.length; i++) {
+        if(ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+        }
+    }
+}
+
+ibg()
